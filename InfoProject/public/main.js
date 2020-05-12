3fbc878a6b0b683300303f95a8e58fca48e1c820
swangkat <62759074+swangkat@users.noreply.github.com>
2020-05-12 09:32:52 -0400

    Delete main.js

--- InfoProject/public/main.js
+++
@@ -1,7 +0,0 @@
-// var url =
-//   "https://livescore-api.com/api-client/leagues/table.json?key=uLglpq0ZNpVU00UK&secret=owoQXfXvrkyPBSj6Dzr20s2ORYIRYDCn&league=74&season=4";
-// fetch(url).then(function (res) {
-//   console.log("HIIIII");
-//   console.log("RSS", res);
-//   return res, json();
-// });
