a18024390df08d2fef2aea4dfcfd1280fd9403c5
swangkat <62759074+swangkat@users.noreply.github.com>
2020-05-11 23:01:18 -0400

    Add files via upload

---
+++ InfoProject/public/main.js
@@ -0,0 +1,7 @@
+// var url =
+//   "https://livescore-api.com/api-client/leagues/table.json?key=uLglpq0ZNpVU00UK&secret=owoQXfXvrkyPBSj6Dzr20s2ORYIRYDCn&league=74&season=4";
+// fetch(url).then(function (res) {
+//   console.log("HIIIII");
+//   console.log("RSS", res);
+//   return res, json();
+// });
